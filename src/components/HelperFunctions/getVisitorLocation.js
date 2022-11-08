8ced4502a7608cc3b9c01f4548b455fc65c9cdd8
Kingsley Onwuchekwa <kingsleykelechi008@gmail.com>
2022-11-08 22:02:46 +0800

    v1.0.0 initial release

--- src/components/HelperFunctions/getVisitorLocation.js
+++ src/components/HelperFunctions/getVisitorLocation.js
@@ -1,28 +1,14 @@
 import getVisitorWeather from "./getVisitorWeather"
-import CountryList from "../CountryList.js"
-
-// function that takes in a country code and returns the country name by matching the code to whatever object sortname property is equal to the code
-const codeToCountry = (code) => {
-    const countryDB = CountryList.data
-    const country = countryDB.find((item) => {
-        return item.sortname.toLowerCase() === code.toLowerCase()
-    })
-    return country.country_name
-}
 
 const getVisitorLocation = async (lat, lon) => {
 const weatherStats = []    
 
-    const url = `https://www.geoplugin.net/extras/location.gp?lat=${lat}&lon=${lon}&format=json`
-    const fetchres = await fetch(url)
-    const data = await fetchres.json()
-    let country = codeToCountry(data.geoplugin_countryCode)
-    weatherStats.push(data.geoplugin_place + ", " + data.geoplugin_region + ", " + country)
+    const url = `/.netlify/functions/fetchLocation?lat=${lat}&lon=${lon}`
+    const fetchres = await fetch(url).then(res => res.json())
+    weatherStats.push(fetchres.results[0].city + ", " + fetchres.results[0].country)
     const weatherInfo = await getVisitorWeather(lat, lon)
   weatherStats.push(weatherInfo)
-
     return weatherStats
-
 
   } 
 export default getVisitorLocation;
